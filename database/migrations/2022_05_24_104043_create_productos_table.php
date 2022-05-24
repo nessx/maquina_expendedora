9bf48df0695d1e739fbd1b415a79010bd9692caf
efidatansantana <nsantana@efidata.es>
2022-05-24 12:50:38 +0200

    fist

---
+++ database/migrations/2022_05_24_104043_create_productos_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre_producto');
+            $table->string('descripcion_producto');
+            $table->string('imagen_producto');
+            $table->string('pais_procedencia');
+            $table-integer('precio_producto');
+            $table->string('kcal');
+            $table->string('informacion_nutricional');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('productos');
+    }
+}
